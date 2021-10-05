fafd224eb285fb8da0a38fc7d4c1d8638967486b
Chithara <chithara.2019@iit.ac.lk>
2021-10-05 17:20:04 +0530

    tutorial1

---
+++ Tutorial1/LotteryProgram/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Random randNum = new Random(); //instance of Random class
+
+        int[] userInputs = new int[6];
+        int[] drawNumbers = new int[6];
+
+        System.out.println("Lottery Game!\n");
+
+        //Get user inputs, store them in an array, and display them
+        for (int i = 0; i < userInputs.length; i++) {
+            System.out.print("Enter lottery number " + (i + 1) + ": ");
+            userInputs[i] = input.nextInt();
+        }
+        for (int i = 0; i < userInputs.length; i++) {
+            System.out.print(userInputs[i] + " ");
+        }
+
+        //Generate 6 random numbers, store them in an array and display them
+        int max = 5;
+        int min = 1;
+        int matches = 0;
+
+        for (int i = 0; i < 6; i++) {
+            drawNumbers[i] = randNum.nextInt(max - min + 1) + min;//numbers between 1 - 49
+        }
+
+        System.out.println("\n");
+
+
+        for (int i = 0; i < drawNumbers.length; i++) {
+            System.out.print(drawNumbers[i] + " ");
+        }
+
+
+        for (int i = 0; i < drawNumbers.length; i++) {
+            for (int j = 0; j < userInputs.length; j++) {
+                if (drawNumbers[i] == userInputs[j]) {
+                    System.out.println(userInputs[j]);
+                    matches++;
+                }
+            }
+        }
+
+        System.out.println("You have " + matches + " matches!");
+    }
+}
