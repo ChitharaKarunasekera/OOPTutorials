951f42914ee971f47623bedb035ff7b30282f0ef
Chithara Karunasekera <78892816+ChitharaKarunasekera@users.noreply.github.com>
2021-10-06 15:31:33 +0530

    Update Main.java

--- Tutorial1/Q1/src/Main.java
+++ Tutorial1/Q1/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Online Learning! \n\n Please virus go awa!");
+        System.out.println("Welcome to Online Learning! \n\n Please virus go aw!");
     }
 }
