ab41150ef2f2c1cdb7124a40145be92af9e99b29
Chithara <chithara.2019@iit.ac.lk>
2021-10-10 18:28:27 +0530

    checking method

--- Tutorial1/LotteryProgram/src/Main.java
+++ Tutorial1/LotteryProgram/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Random randNum = new Random(); //instance of Random class
         System.out.println("Lottery Game!\n");
 
-        int[] inputs = new int[3];
+        int[] inputs = new int[6];
         int[] luckyNums = new int[6];
 
         //Get a set of user numbers
@@ -16,42 +16,62 @@ public class Main {
 
         //Display user numbers
         System.out.println("\nYour Values,");
-        for (int i=0; i< inputs.length; i++){
-            System.out.print(randNum.nextInt(5 - 1 + 1) + 1);
+        for (int i : inputs) {
+            System.out.print(i + " ");
         }
         System.out.println("");
 
 
         //Generate the 6 Lucky numbers
-        //setLuckyNums(luckyNums);
+        setLuckyNums(luckyNums);
 
         //Display the luck numbers
         System.out.println("\nLucky Numbers,");
-        for (int i=0; i< luckyNums.length; i++){
-            System.out.print(luckyNums[i] + " ");
+        for (int i : luckyNums) {
+            System.out.print(i + " ");
         }
-        System.out.println("");
+        System.out.println("\n");
+
+        System.out.println("Results.");
+        checkMatch(inputs, luckyNums);
     }
 
     public static void setUserNumbers(int[] refInputs) {
         Scanner input = new Scanner(System.in);
 
         for (int i = 0; i < refInputs.length; i++) {
-            System.out.print("Enter lucky number " + (i+1) + " : ");
+            System.out.print("Enter lucky number " + (i + 1) + " : ");
             refInputs[i] = input.nextInt();
         }
     }
 
-//    public static void setLuckyNums(int[] refLuckyNums){
-//        Random randNum = new Random(); //instance of Random class
-//
-//        int max = 1;
-//        int min = 5;
-//
-//        for (int i=0; i< refLuckyNums.length; i++){
-//            refLuckyNums[i] = randNum.nextInt(max - min + 1) + min;
-//        }
-//    }
+    public static void setLuckyNums(int[] refLuckyNums) {
+        Random randNum = new Random(); //instance of Random class
+
+        int max = 5;
+        int min = 1;
+
+        for (int i = 0; i < refLuckyNums.length; i++) {
+            refLuckyNums[i] = randNum.nextInt(max - min + 1) + min;
+        }
+    }
+
+    public static void checkMatch(int[] refInputs, int[] refLuckyNums) {
+        int count = 0;
+
+        //tack one input at time
+        for (int checkNum : refInputs) {
+            for (int currentElement : refLuckyNums) {
+                if (currentElement == checkNum) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Total Matches : " + count);
+    }
+
 }
 
 
