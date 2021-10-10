1a2b29ad5ca1794b19c9286d12f5ee3f761b3c42
Chithara <chithara.2019@iit.ac.lk>
2021-10-10 21:37:43 +0530

    Mississippi

---
+++ Tutorial2/Mississippi/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Code from tutorial
+        String river = "Mississippi";
+        String bigRiver = river.toUpperCase();
+        System.out.println(bigRiver);
+
+        //Modified
+        bigRiver = river.toLowerCase();
+        System.out.println(bigRiver);
+    }
+}
