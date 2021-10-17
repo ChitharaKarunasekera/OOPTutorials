381eaaab7e846e9852b286d076a452899b3b6992
Chithara Karunasekera <78892816+ChitharaKarunasekera@users.noreply.github.com>
2021-10-17 16:17:32 +0530

    Update Main.java

--- Tutorial1/LotteryProgram/src/Main.java
+++ Tutorial1/LotteryProgram/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Man {
+public class Main {
 
     public static void main(String[] args) {
         Random randNum = new Random(); //instance of Random class
