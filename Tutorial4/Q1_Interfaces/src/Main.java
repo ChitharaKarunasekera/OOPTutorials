b5962e8fc3f14353d35e7318883720675636cb97
Chithara <chithara.2019160@iit.ac.lk>
2021-10-29 15:19:49 +0530

    added to Main

--- Tutorial4/Q1_Interfaces/src/Main.java
+++ Tutorial4/Q1_Interfaces/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        String[] grade = {"a","a","a","a","a"};
+
         Printable p1 = new Letter("Hello this is chithara's letter!");
+        Printable p2 = new Student("Chithara", grade);
 
         p1.print();
+        p2.print();
     }
 }
