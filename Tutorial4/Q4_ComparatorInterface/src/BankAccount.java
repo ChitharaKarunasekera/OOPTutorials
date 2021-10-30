a909417854e3091f4ba29ef917b05fe747d03294
Chithara <chithara.2019160@iit.ac.lk>
2021-10-30 15:46:07 +0530

    added files

---
+++ Tutorial4/Q4_ComparatorInterface/src/BankAccount.java
@@ -0,0 +1,31 @@
+public class BankAccount implements Comparable<BankAccount>{
+
+    private double balance;
+
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+    /**
+     * Compares two bank accounts.
+     *
+     * @param other the other BankAccount
+     * @return 1 if this bank account has a greater balance than the other one,
+     * -1 if this bank account is has a smaller balance than the other one,
+     * and 0 if both bank accounts have the same balance
+     */
+    public int compareTo(BankAccount other) {
+        if (this.balance > other.balance) {
+            return 1;
+        } else if (this.balance < other.balance) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+}
