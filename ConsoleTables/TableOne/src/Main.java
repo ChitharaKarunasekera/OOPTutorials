564ca0c31f3f40b41ce47795e573cdd9da639b8f
Chithara <chithara.2019160@iit.ac.lk>
2021-11-06 19:34:45 +0530

    added a table

--- ConsoleTables/TableOne/src/Main.java
+++ ConsoleTables/TableOne/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int id1 = 3456, id2 = 5757;
+        String name1 = "Saad Yousuf", name2 = "John Petersen";
+        double gpa1 = 3.91, gpa2 = 3.9;
+        System.out.printf("%-6s%-20s%6s\n", "IDs","Names","GPAs");
+        System.out.printf("%-6d%-20s%6.2f\n", id1,name1,gpa1);
+        System.out.printf("%-6d%-20s%6.2f\n", id2, name2, gpa2);
     }
 }
