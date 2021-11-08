3b4262683242ffcda6c221a6be59e4ae915b0688
Chithara <chithara.2019160@iit.ac.lk>
2021-11-08 11:13:40 +0530

    completed Q7

---
+++ Tutorial4/Q7_Polymorphism/src/C.java
@@ -0,0 +1,5 @@
+public class C extends A{
+    void print() {
+        System.out.println("print C!");
+    }
+}
---
+++ Tutorial4/Q7_Polymorphism/src/D.java
@@ -0,0 +1,5 @@
+public class D extends  B{
+    void print() {
+        System.out.println("print D!");
+    }
+}
