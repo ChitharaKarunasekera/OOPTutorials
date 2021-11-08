1e8fdc1ca418aa6989a33e06926b154263881fcc
Chithara <chithara.2019160@iit.ac.lk>
2021-11-08 13:22:18 +0530

    improved code

--- Tutorial4/Q3_Comparable Interface/src/Main.java
+++ Tutorial4/Q3_Comparable Interface/src/Main.java
@@ -7,12 +7,16 @@ public class Main {
         BankAccount ba1 = new BankAccount(1000.0);
         BankAccount ba2 = new BankAccount(500.0);
         BankAccount ba3 = new BankAccount(2000.0);
+        BankAccount ba4 = new BankAccount(100.0);
+        BankAccount ba5 = new BankAccount(3000.0);
 
         // put bank accounts into a list
         ArrayList<BankAccount> list = new ArrayList<BankAccount>();
         list.add(ba1);
         list.add(ba2);
         list.add(ba3);
+        list.add(ba4);
+        list.add(ba5);
 
         // call the library sort method
         Collections.sort(list);
