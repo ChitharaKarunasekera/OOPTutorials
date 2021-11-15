cfefb4fd30174f322dcd1f5b41acf2c5af2cede2
Chithara <chithara.2019160@iit.ac.lk>
2021-11-15 12:33:18 +0530

    complete Q1

--- Tutorial6/Two-dimensionalArrays/src/Board.java
+++ Tutorial6/Two-dimensionalArrays/src/Board.java
@@ -1,3 +1,4 @@
+import javax.naming.PartialResultException;
 import java.util.Random;
 
 public class Board {
@@ -18,9 +19,50 @@ public class Board {
     // display the contents of the board
     public void print() {
         for (int[] r : ar) { // for all rows
-            for (int c : r) // for every element in current row
-                System.out.print(c + " ");
+            for (int c : r) { // for every element in current row
+                System.out.print(c + "\t");
+            }
             System.out.println();
         }
     }
+
+    //Finding the maximum value in a row
+    public int findMaxInRow(int row) {
+        int max;
+        int[] r = ar[row];
+        max = r[0];
+
+        for (int i : r) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    //find maximum value in a column
+    public int findMaxInColumn(int column) {
+        int max = 0;
+
+        for (int[] ints : ar) {
+            if (max < ints[column]) {
+                max = ints[column];
+            }
+        }
+        return max;
+    }
+
+    //find maximum value in the diagonal
+    public int findMaxInDiagonal() {
+        int max = 0;
+
+        for (int i = 0; i < ar.length; i++) {
+            if (max < ar[i][i]) {
+                max = ar[i][i];
+            }
+        }
+
+        return max;
+    }
+
 }
