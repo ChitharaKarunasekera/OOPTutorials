d1a59fe0becbc7c3f76e791ea797909d33e7c00d
Chithara <chithara.2019160@iit.ac.lk>
2021-11-15 12:56:46 +0530

    Q3

---
+++ Tutorial6/Q3_Generics-ArrayLists/src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    public String author;
+    public String title;
+
+    public Book(String author, String title){
+        this.author = author;
+        this.title = title;
+    }
+}
