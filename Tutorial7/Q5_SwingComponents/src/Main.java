ba9ed4239f914c2da8343df918417115d3e92ee9
Chithara <chithara.2019160@iit.ac.lk>
2021-11-26 23:02:41 +0530

    Changed text

--- Tutorial7/Q5_SwingComponents/src/Main.java
+++ Tutorial7/Q5_SwingComponents/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("ComponentExample");
-        JButton button = new JButton("press me");
+        JButton button = new JButton("Press me");
         JPanel jp = new JPanel();
         jp.setBackground(Color.white);
 
