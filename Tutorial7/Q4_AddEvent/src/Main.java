4719594a60ab75da43ae13499ea2b5e795412977
Chithara <chithara.2019160@iit.ac.lk>
2021-12-05 21:52:39 +0530

    Update Main.java

--- Tutorial7/Q4_AddEvent/src/Main.java
+++ Tutorial7/Q4_AddEvent/src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("SimpleSwingExample");
+        JFrame frame = new JFrame("Simple SwingExample");
 
         // register an event handler for frame events
         frame.addWindowListener(new MyWindowListener());
