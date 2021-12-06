9aa71b919672ee714e59b19c7a28c0ed77233fda
Chithara <chithara.2019160@iit.ac.lk>
2021-12-06 12:41:48 +0530

    added functionality

--- Tutorial7/Q11_ButtonGroup/src/MyCheckBoxListener.java
+++ Tutorial7/Q11_ButtonGroup/src/MyCheckBoxListener.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+import java.awt.event.ItemEvent;
+
 public class MyCheckBoxListener {
+    public void itemStateChanged(ItemEvent e) {
+        JCheckBox chk = (JCheckBox) e.getItem();
+        String label = chk.getText();
+        if (e.getStateChange() == e.SELECTED)
+            System.out.println(label + " selected");
+        else
+            System.out.println(label + " de-selected");
+    }
+
 }
