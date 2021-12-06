a4b0f82182a0e102e324e7a059dd21fc17beb145
Chithara <chithara.2019160@iit.ac.lk>
2021-12-06 12:41:14 +0530

    added new class

--- Tutorial7/Q11_ButtonGroup/src/MyActionListener.java
+++ Tutorial7/Q11_ButtonGroup/src/MyActionListener.java
@@ -1,3 +1,7 @@
+import java.awt.event.ActionEvent;
+
 public class MyActionListener {
-    
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Selected: " + e.getActionCommand());
+    }
 }
