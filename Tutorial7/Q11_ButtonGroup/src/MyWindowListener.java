45d796d608c228fc35cdced4f12e2a26f0f4a64d
Chithara <chithara.2019160@iit.ac.lk>
2021-12-06 12:31:57 +0530

    New class added

--- Tutorial7/Q11_ButtonGroup/src/MyWindowListener.java
+++ Tutorial7/Q11_ButtonGroup/src/MyWindowListener.java
@@ -1,2 +1,8 @@
+import java.awt.event.WindowEvent;
+
 public class MyWindowListener {
+    public void windowClosing(WindowEvent e) {
+        System.out.println("Closing window!");
+        System.exit(0);
+    }
 }
