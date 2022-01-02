2e07881e10ec7d6e0d1fcfeea3b939a0863968bd
Chithara <chithara.2019160@iit.ac.lk>
2022-01-02 22:02:21 +0530

    extended random player

--- Tic-Tac-To Game/Tic-Tac-Toe/src/IntelligentPlayer.java
+++ Tic-Tac-To Game/Tic-Tac-Toe/src/IntelligentPlayer.java
@@ -1,3 +1,6 @@
 //computer that plays based on the users input. Has the ability to defend
-public class IntelligentPlayer {
+public class IntelligentPlayer extends RandomPlayer{
+    IntelligentPlayer(Board board, char symbol) {
+        super(board, symbol);
+    }
 }
