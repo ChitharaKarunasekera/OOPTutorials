6add6f0fc3537afc616b0802694b1b735f3b50bf
Chithara <chithara.2019160@iit.ac.lk>
2022-01-03 21:50:15 +0530

    added basic act method

--- Tic-Tac-To Game/Tic-Tac-Toe/src/IntelligentPlayer.java
+++ Tic-Tac-To Game/Tic-Tac-Toe/src/IntelligentPlayer.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 //computer that plays based on the users input. Has the ability to defend
 public class IntelligentPlayer extends RandomPlayer{
     IntelligentPlayer(Board board, char symbol) {
         super(board, symbol);
+    }
+
+    // takes an action based on the current state of the Board
+    public String act() {
+        System.out.println(board); // display the current state of the board
+        System.out.println("Machine is thinking...");
+
+        // simulate thinking by delaying a bit
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        // find all available actions (empty slots)
+        ArrayList<Integer> available = new ArrayList<>();
+        for (int i = 0; i < board.current_state.length(); i++)
+            if (board.current_state.charAt(i) == '-') {
+                available.add(i);
+            }
+
+        // choose a random action among the available empty slots
+        Random gen = new Random();
+        int index = gen.nextInt(available.size());
+        System.out.println("*** Played position-> " + (1 + available.get(index)) + "\n");
+
+        // numbering scheme starts at 1
+        return String.valueOf(1 + available.get(index));
     }
 }
