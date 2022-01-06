d67078438d8a0163a1a1896064059b50fe6ec512
Chithara <chithara.2019160@iit.ac.lk>
2022-01-06 21:44:10 +0530

    Added code

--- Tutorial9/Q8/src/Main.java
+++ Tutorial9/Q8/src/Main.java
@@ -1,6 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public static void cat(File file) throws IOException {
+        RandomAccessFile input = null;
+        String line = null;
+
+        try {
+            input = new RandomAccessFile(file, "r");
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
     }
 }
